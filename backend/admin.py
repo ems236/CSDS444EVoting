b08da39a628df0b2ffae7e88c72e08263bf1ad91
Imran Hossain <contact@imranhossa.in>
2020-11-25 15:21:55 -0500

    Clean stuff up

--- backend/admin.py
+++ backend/admin.py
@@ -1,4 +1,4 @@
-from utils.rsa import genrsa, verify_fdh, blind_sign
+from utils.rsa import verify_fdh, blind_sign
 
 class Administrator:
 
@@ -6,30 +6,26 @@ class Administrator:
     Creates an Administrator instance with list of voters and sets up RSA keys
 
     voters_list: dictionary of registered voters with voter_id as a key and public key pair as a value
-    signed_list: list of ballots already signed
     """
-    def __init__(self, voters_list, signed_list):
-    	N, publicExponent, privateExponent = genrsa()
-    	self.N = N
-    	self.publicExponent = publicExponent
-    	self.privateExponent = privateExponent
-    	self.voters_list = voters_list
-   		self.signed_list = signed_list
+    def __init__(self, rsa_keypair, voters_list):
+        self.N, self.publicExponent, self.privateExponent = rsa_keypair
+        self.voters_list = voters_list
+        self.signed_list = []
 
     """
     Returns tuple of public keys, N and public Exponent
     """
     def get_public_keys(self):
-    	return self.N, self.publicExponent
+        return self.N, self.publicExponent
 
 
     """
-	Given encrypted vote with signature, verify the vote.
+    Given encrypted vote with signature, verify the vote.
 
-	Returns blinded ballot if the voter is registered, hasn't submitted ballot, and signature is valid
-	returns None otherwise.
+    Returns blinded ballot if the voter is registered, hasn't submitted ballot, and signature is valid
+    returns None otherwise.
 
-	input: vote is a tuple consisting of (voter_id, encrypted vote, signature)
+    input: vote is a tuple consisting of (voter_id, encrypted vote, signature)
     """
     def certify_vote(self, vote: tuple):
         """Certify a given vote by running it through multiple checks.
@@ -37,29 +33,26 @@ class Administrator:
 
         # checking the input format
         try:
-	        voter_id = vote[0]
-	        encrypted_vote = vote[1]
-	        signature = vote[2]
-	        voter_N = self.voters_list[voter_id][0]
-	        voter_exponent = self.voters_list[voter_id][1]
-	    except IndexError as err1:
-	    	print("The vote input is missing some elements")
-	    	raise err1
-	    except ValueError as err2:
-	    	print("No public keys found for given voter")
-	    	raise err2
+            voter_id = vote[0]
+            encrypted_vote = vote[1]
+            signature = vote[2]
+            voter_N = self.voters_list[voter_id][0]
+            voter_exponent = self.voters_list[voter_id][1]
+        except IndexError as err1:
+            print("The vote input is missing some elements")
+            raise err1
+        except ValueError as err2:
+            print("No public keys found for given voter")
+            raise err2
 
-	    # TODO: check whether the input vote will be the same format as returned value of Voter.cast()
-	    if voter_id is in self.voters_list \
-	    	and vote in not in self.signed_list \
-	    	and verify_fdh(encrypted_vote, signature, voter_exponent, voter_N):
+        # TODO: check whether the input vote will be the same format as returned value of Voter.cast()
+        if voter_id in self.voters_list \
+            and vote not in self.signed_list \
+            and verify_fdh(encrypted_vote, signature, voter_exponent, voter_N):
 
-	    	self.signed_list.append(vote)
+            self.signed_list.append(vote)
 
-	    	return blind_sign(encrypted_vote, self.privateExponent, self.N)
+            return blind_sign(encrypted_vote, self.privateExponent, self.N)
 
-	    else:
-	    	return None
-
-
-
+        else:
+            return None
--- backend/counter.py
+++ backend/counter.py
@@ -1,20 +1,19 @@
-from ballot import Vote
 from utils.rsa import verify_fdh, xor
 
 class Counter:
 
-	"""
-	Creates a Counter instance given:
-		voters_list: dictionary of voter_id to public keys
-		signer_public_keys: tuple containing signer's public keys (N, exponent)
-		received_votes: list of votes already received
-		unlocked_votes: dictionary of index to unlocked votes TODO: should we use array of byte arrays? or call this good enough
-	"""
-    def __init__(self, voters_list, signer_public_keys, received_votes, unlocked_votes):
-    	self.voters_list = voters_list
-    	self.signer_public_keys = signer_public_keys
-    	self.received_votes = received_votes
-    	self.unlocked_votes = unlocked_votes
+    """
+    Creates a Counter instance given:
+        voters_list: dictionary of voter_id to public keys
+        admin_public_keys: tuple containing admin's public keys (N, exponent)
+        received_votes: list of votes already received
+        unlocked_votes: dictionary of index to unlocked votes TODO: should we use array of byte arrays? or call this good enough
+    """
+    def __init__(self, voters_list, admin_public_keys):
+        self.voters_list = voters_list
+        self.signer_public_keys = admin_public_keys
+        self.received_votes = []
+        self.unlocked_votes = {}
 
 
     """
@@ -27,19 +26,19 @@ class Counter:
         """
 
         try:
-        	vote = vote_sign_pair[0]
-        	signature = vote_sign_pair[1]
-        	signer_N = self.signer_public_keys[0]
-        	signer_exponent = self.signer_public_keys[1]
+            vote = vote_sign_pair[0]
+            signature = vote_sign_pair[1]
+            signer_N = self.signer_public_keys[0]
+            signer_exponent = self.signer_public_keys[1]
         except IndexError as err:
-        	print("vote_sign_pair or signer_public_keys not containing enough values")
-        	raise err
+            print("vote_sign_pair or signer_public_keys not containing enough values")
+            raise err
 
         if verify_fdh(vote, signature, signer_exponent, signer_N):
-        	self.received_votes.append(vote_sign_pair)
-        	return True
+            self.received_votes.append(vote_sign_pair)
+            return True
         else:
-        	return False
+            return False
 
 
 
@@ -48,15 +47,15 @@ class Counter:
         then add it to the list of unlocked votes.
         """
         try:
-        	index = index_key_pair[0]
-        	key = index_key_pair[1]
+            index = index_key_pair[0]
+            key = index_key_pair[1]
         except IndexError as err:
-        	print("missing either index or key in the input")
-        	raise err
+            print("missing either index or key in the input")
+            raise err
 
         if index >= len(self.received_votes):
-        	print("vote not received previously")
-        	raise IndexError()
+            print("vote not received previously")
+            raise IndexError()
 
         vote = self.received_votes[index]
 
@@ -64,8 +63,8 @@ class Counter:
         revealed_vote = xor(vote, key)
 
         """
-		TODO check the unlocked vote's validity 
-		(my understanding is that we need to know the expected format of the vote to do this)
+        TODO check the unlocked vote's validity 
+        (my understanding is that we need to know the expected format of the vote to do this)
         """
 
         #adding revealed vote to the list
